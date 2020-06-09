e208c9650b3fb50f6a4d37a5ab01ed0dee9be5b4
edgarlsx <edgarlsx@hotmail.com>
2020-06-08 23:31:53 -0300

    Classes Categoria e Contexto, configuração do Banco de Dados, Migrations e Controle Categorias.

---
+++ Models/Contexto.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CursoMVC_Carrefour.Models
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Categoria> Categorias { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString: @"Data Source=(LocalDB)\MSSQLLocalDB;Database=CursoMVC-Carrefour;Integrated Security=True");
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CursoMVC_Carrefour.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -24,6 +25,9 @@ namespace CursoMVC_Carrefour
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<Contexto>();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
